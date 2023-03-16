d78baf7f7e69e2fadeaa8b5abae5bd07b2f03384
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-03-16 22:40:45 +0900

    graphql環境構築

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      cors: {
+        origin: '*',
+      },
+    }),
+  ],
 })
 export class AppModule {}
